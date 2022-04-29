4f36a1aaa0fb1fe7fe761856fb1a0d3af5039fe3
Sixels <sixels@protonmail.com>
2022-04-29 16:07:04 -0300

    Add custom events

---
+++ src/events.ts
@@ -0,0 +1,57 @@
+import { WordAttempt } from "./game";
+import { BoardPosition } from "./game/board";
+import { Message } from "./message";
+
+export default {
+  dispatchSendKeyEvent: (key: string) => {
+    let send_key = new CustomEvent("sendkey", {
+      detail: {
+        key,
+      },
+      bubbles: false,
+      cancelable: false,
+      composed: true,
+    });
+
+    document.dispatchEvent(send_key);
+  },
+
+  dispatchSetPositionEvent: (position: BoardPosition) => {
+    let set_position = new CustomEvent("setposition", {
+      detail: {
+        position,
+      },
+      bubbles: false,
+      cancelable: false,
+      composed: false,
+    });
+
+    document.dispatchEvent(set_position);
+  },
+
+  dispatchWordAttemptEvent: (attempt_desc: WordAttempt) => {
+    let word_attempt = new CustomEvent("wordattempt", {
+      detail: {
+        attempt_desc,
+      },
+      bubbles: true,
+      cancelable: false,
+      composed: false,
+    });
+
+    document.dispatchEvent(word_attempt);
+  },
+
+  dispatchSendMessageEvent: (message: Message) => {
+    let send_message = new CustomEvent("sendmessage", {
+      detail: {
+        message,
+      },
+      bubbles: true,
+      cancelable: false,
+      composed: false,
+    });
+
+    document.dispatchEvent(send_message);
+  }
+};
