503a295deb91bafc966cd01517f4445a790b7892
Sixels <sixels@protonmail.com>
2022-04-29 16:06:18 -0300

    Handle keyboard events

---
+++ src/keyboard.ts
@@ -0,0 +1,129 @@
+import events from "./events";
+import { WordAttempt } from "./game";
+
+export class KeyboardManager {
+  static VALID_KEYS = new Set([
+    "enter",
+    "backspace",
+    "arrowleft",
+    "arrowright",
+    ..."qwertyuiopasdfghjklzxcvbnm".split(""),
+  ]);
+
+  private keys: HTMLElement[] = [];
+  private effectTimeouts = new Map();
+
+  // Create a new keyboard manager
+  constructor(elem: HTMLElement) {
+    // handle clicks from the virtual keyboard
+    const rows: HTMLElement[] = [].slice.call(elem.children);
+    for (const row of rows) {
+      const keys: HTMLElement[] = [].slice.call(row.children);
+      for (const key of keys) {
+        key.addEventListener("click", this.handleKeyClick);
+      }
+
+      this.keys = [...this.keys, ...keys];
+    }
+    document.addEventListener("wordattempt", this.handleWordAttempt);
+  }
+
+  // Check wether a key is valid or not
+  static isKeyValid(key: string): boolean {
+    return KeyboardManager.VALID_KEYS.has(key);
+  }
+
+  // Propagate the key event to the GameManager's queue
+  pressKey = (key: string) => {
+    key = key.toLocaleLowerCase();
+    this.highlightKey(key);
+    if (KeyboardManager.isKeyValid(key)) {
+      events.dispatchSendKeyEvent(key);
+    }
+  };
+
+  // Apply a highlight effect to the given key
+  private highlightKey = (key: string) => {
+    // avoid searching for invalid characters
+    if (KeyboardManager.isKeyValid(key) === false) {
+      return;
+    }
+
+    const key_element = this.keys.find((v) => {
+      return key === v.dataset["key"];
+    });
+
+    if (key_element !== undefined) {
+      key_element.classList.add("highlighted");
+
+      let prevTimeout = this.effectTimeouts.get(key);
+      if (prevTimeout !== null) {
+        this.effectTimeouts.delete(key);
+        clearTimeout(prevTimeout);
+      }
+
+      let timeout = setTimeout(() => {
+        key_element.classList.remove("highlighted");
+        this.effectTimeouts.delete(key);
+      }, 150);
+
+      this.effectTimeouts.set(key, timeout);
+    }
+  };
+
+  // Handle keys from the virtual keyboard
+  private handleKeyClick = (event: MouseEvent | TouchEvent) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    const target = event.target as HTMLElement | null;
+
+    if (target !== null) {
+      const key = target.dataset["key"];
+      if (key !== undefined) {
+        this.pressKey(key);
+      }
+    }
+  };
+
+  private handleWordAttempt = (event: Event) => {
+    const custom_ev = event as CustomEvent;
+    const attempt_desc = custom_ev.detail["attempt_desc"] as WordAttempt | null;
+
+    if (attempt_desc === null) {
+      return;
+    }
+
+    for (const key of this.keys) {
+      if (
+        attempt_desc.wrong_letters.some(
+          (v) => v.normalized == key.dataset["key"]
+        )
+      ) {
+        key.classList.add("wrong");
+        continue;
+      }
+      if (
+        attempt_desc.right_letters.some(
+          (v) => v.normalized == key.dataset["key"]
+        )
+      ) {
+        key.classList.add("right");
+        continue;
+      }
+      if (
+        !key.classList.contains("right") &&
+        attempt_desc.occur_letters.some(
+          (v) => v.normalized == key.dataset["key"]
+        )
+      ) {
+        key.classList.add("occur");
+      }
+    }
+  };
+
+  // Handle keys from the physical keyboard
+  handleKeyPress = (event: KeyboardEvent) => {
+    this.pressKey(event.key);
+  };
+}
