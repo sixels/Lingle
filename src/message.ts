ae386cd628629cae72e666d87c479ae936293d77
Sixels <sixels@protonmail.com>
2022-05-23 18:07:25 -0300

    Add on_click to message

--- src/message.ts
+++ src/message.ts
@@ -6,8 +6,9 @@ export enum MessageKind {
 export interface Message {
   kind: MessageKind;
   data: string;
-  callback: (() => void) | undefined;
   timeout: number | undefined;
+  callback: (() => void) | undefined;
+  on_click: (() => void) | undefined;
 }
 
 export const messages = {
@@ -16,43 +17,38 @@ export const messages = {
       kind: MessageKind.Error,
       data: `A palavra deve ter ${expected_size} letras.`,
       callback,
-      timeout: undefined,
-    };
+    } as Message;
   },
   invalidWord: (callback?: () => void): Message => {
     return {
       kind: MessageKind.Error,
       data: "Palavra não reconhecida.",
       callback,
-      timeout: undefined,
-    };
+    } as Message;
   },
   gameWin: (callback?: () => void): Message => {
     return {
       kind: MessageKind.Info,
       data: "Parabéns, você acertou!",
       callback,
-      timeout: undefined,
-    };
+    } as Message;
   },
   gameLost: (solutions: string[], callback?: () => void): Message => {
     const message =
       solutions.length > 1
         ? "As palavras de hoje eram"
-        : "A palavra de hoje era";
+        : "A palavra de hoje erX";
     return {
       kind: MessageKind.Info,
       data: `Você perdeu. ${message}: ${solutions.join(", ")}.`,
       callback,
-      timeout: undefined,
-    };
+    } as Message;
   },
   resultCopied: (callback?: () => void): Message => {
     return {
       kind: MessageKind.Info,
       data: "Resultado copiado para área de transferência. Use Ctrl+V para colar.",
       callback,
-      timeout: undefined,
-    };
+    } as Message;
   },
 };
