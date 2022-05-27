9822fe6030b8fb3cfe4f2303f20508f467207ba6
Sixels <sixels@protonmail.com>
2022-05-27 20:11:04 -0300

    Add custom import paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import ViteSolid from "vite-plugin-solid";
 
-const app = require("./package.json");
+import app from "./package.json";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   publicDir: "static",
   build: {
     target: "esnext",
