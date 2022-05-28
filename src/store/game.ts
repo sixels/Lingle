530f46cd2c3320e33067554cf2c4cea03b74679e
Sixels <sixels@protonmail.com>
2022-05-28 14:10:19 -0300

    Add store methods

--- src/store/game.ts
+++ src/store/game.ts
@@ -1,21 +1,59 @@
+import { GameStatus, WordAttempt } from "@/game";
+import { BoardPosition } from "@/game/board";
+import { Mode, Modes } from "@/game/mode";
 import utils from "@/utils";
 
-export interface GameStats {}
+export type GameStore = [GameState, GameStoreMethods];
 
-export interface GameState {}
-
-export interface GameStore {
-  stats: GameStats;
-  state: GameState;
+export interface GameState {
+  stats: IGameStats;
+  state: IGameState;
   solutions: string[];
   expires: Date;
+  mode: Modes;
 }
 
-export const defaultGameStore = (): GameStore => {
+export interface GameStoreMethods {
+  setMode: (mode: Mode) => void;
+  setPosition: (position: BoardPosition) => void;
+  setBoardStatus: (board: number, status: GameStatus) => void;
+}
+
+interface IGameStats {}
+
+interface IGameState {
+  position: [number, number];
+  game_number: number;
+  boards: {
+    status: GameStatus;
+    attempts: WordAttempt[];
+    solution: string | undefined;
+  }[];
+}
+
+/**
+ * Get the default state for the given mode
+ * @param mode The game mode
+ * @returns A GameState
+ */
+export const defaultGameStore = (mode: Mode): GameState => {
+  const makeBoards = (mode: Mode): IGameState["boards"] => {
+    let boards: IGameState["boards"] = [];
+    for (let i = 0; i < mode.boards; i++) {
+      boards.push({ status: "playing", attempts: [], solution: undefined });
+    }
+    return boards;
+  };
+
   return {
     stats: {},
-    state: {},
+    state: {
+      boards: makeBoards(mode),
+      game_number: 0,
+      position: [0, 0],
+    },
     solutions: [],
     expires: utils.tomorrow(),
+    mode: mode.mode,
   };
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,39 +1,107 @@
 import { createEffect } from "solid-js";
 import { createStore, SetStoreFunction } from "solid-js/store";
 
-import utils from "@/utils";
-import { defaultGameStore, GameStore } from "./game";
-import { defaultPrefsStore } from "./prefs";
+import { Mode, Modes } from "@/game/mode";
+import { BoardPosition } from "@/game/board";
+import { GameStatus } from "@/game";
+import { defaultGameStore, GameState, GameStore } from "./game";
+import { defaultPrefsStore, PrefsStore } from "./prefs";
 
-const STORE_LINGLE_KEY: string = "lingle.v2" as const;
-const STORE_DUOLINGLE_KEY: string = "duolingle.v2" as const;
-const STORE_PREFS_KEY: string = "prefs.v2" as const;
+const STORE_LINGLE_KEY: string = "v2.lingle.normal" as const;
+const STORE_DUOLINGLE_KEY: string = "v2.lingle.duo" as const;
+const STORE_PREFS_KEY: string = "v2.prefs" as const;
 
-function getOrInsertWith<T>(
-  key: string,
-  init_callback: () => T
-): [T, SetStoreFunction<T>] {
-  const stored = localStorage.getItem(key),
-    store = createStore(stored ? (JSON.parse(stored) as T) : init_callback());
+export interface LingleStore {
+  game: GameStore;
+  prefs: [PrefsStore, any];
+}
+
+interface AppState {
+  game: [GameState, SetStoreFunction<GameState>];
+  prefs: [PrefsStore, SetStoreFunction<PrefsStore>];
+}
+
+function storageKeyFromMode(mode: Modes): string {
+  const table = {
+    lingle: STORE_LINGLE_KEY,
+    duolingle: STORE_DUOLINGLE_KEY,
+  };
+  return table[mode];
+}
+
+function getOrElse<T extends Object>(key: string, fallback: T): T {
+  const stored = localStorage.getItem(key);
+  return stored ? ({ ...fallback, ...JSON.parse(stored) } as T) : fallback;
+}
+
+function makeStore<T>(value: T): [T, SetStoreFunction<T>] {
+  const store = createStore(value);
   return store;
 }
 
-function createAppState() {
-  const lingle = getOrInsertWith(STORE_LINGLE_KEY, defaultGameStore);
-  const duolingle = getOrInsertWith(STORE_DUOLINGLE_KEY, defaultGameStore);
-  const prefs = getOrInsertWith(STORE_PREFS_KEY, defaultPrefsStore);
+function createAppState(
+  game_state: GameState,
+  prefs_state: PrefsStore
+): AppState {
+  const [game, setGame] = makeStore(
+    getOrElse(storageKeyFromMode(game_state.mode), game_state)
+  );
+  const [prefs, setPrefs] = makeStore(getOrElse(STORE_PREFS_KEY, prefs_state));
 
   createEffect(() => {
-    localStorage.setItem(STORE_LINGLE_KEY, JSON.stringify(lingle[0]));
-    localStorage.setItem(STORE_DUOLINGLE_KEY, JSON.stringify(duolingle[0]));
-    localStorage.setItem(STORE_PREFS_KEY, JSON.stringify(prefs[0]));
+    localStorage.setItem(storageKeyFromMode(game.mode), JSON.stringify(game));
+    localStorage.setItem(STORE_PREFS_KEY, JSON.stringify(prefs));
   });
 
   return {
-    lingle,
-    duolingle,
-    prefs,
+    game: [game, setGame],
+    prefs: [prefs, setPrefs],
   };
 }
 
-export {};
+export function createLingleStore(mode: Mode): LingleStore {
+  // create state with default values
+  const state = createAppState(defaultGameStore(mode), defaultPrefsStore());
+
+  const [game, setGame] = state.game;
+  const [prefs, setPrefs] = state.prefs;
+
+  return {
+    game: [
+      game,
+      {
+        setMode: (mode: Mode) => {
+          if (game.mode === mode.mode) {
+            return;
+          }
+
+          setGame(
+            getOrElse(storageKeyFromMode(mode.mode), defaultGameStore(mode))
+          );
+        },
+        setPosition: (position: BoardPosition) => {
+          const rc = position.asTuple();
+          setGame({ state: { ...game.state, position: rc } });
+        },
+        setBoardStatus: (board: number, status: GameStatus) => {
+          if (board >= game.state.boards.length) {
+            return;
+          }
+
+          const state = { ...game.state };
+          state.boards[board].status = status;
+
+          setGame({ state });
+        },
+      },
+    ],
+    prefs: [
+      prefs,
+      {
+        setTheme: () => {
+          // TODO
+        },
+      },
+    ],
+  };
+}
