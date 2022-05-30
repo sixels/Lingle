980b8ea0da0eeebe35afc1903da3d27b09584f4c
Sixels <sixels@protonmail.com>
2022-05-30 13:25:51 -0300

    Init keyboard provider and pass it to elements

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,32 @@
 import { Component } from "solid-js";
 
-import Board from "./Board";
+import Board from "./board";
 import Keyboard from "./Keyboard";
 import Header from "./header";
 
 import { createLingleStore } from "@/store";
 import { Mode } from "@/game/mode";
 
+import keyboard from "../keyboardProvider";
+
 const default_mode = new Mode("lingle");
 
 const App: Component = () => {
   const {
-    game: [game, { setMode }],
+    game: [game, { setMode, setRow, createAttempt }],
     // prefs: [prefs, prefsMethods],
   } = createLingleStore(default_mode);
 
   return (
     <>
       <Header gameState={game} setMode={setMode} />
-      <Board />
-      <Keyboard />
+      <Board
+        gameState={game}
+        keyboard={keyboard}
+        setRow={setRow}
+        createAttempt={createAttempt}
+      />
+      <Keyboard keyboard={keyboard} />
     </>
   );
 };
