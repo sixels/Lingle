1bdf24a6103ccff2d26cfa0cab07880064781c12
Sixels <sixels@protonmail.com>
2022-06-08 14:53:17 -0300

    Fix makeHistory

--- src/store/game/index.ts
+++ src/store/game/index.ts
@@ -48,8 +48,9 @@ export const defaultGameState = (mode: Mode): GameState => {
   const makeHistory = (): IGameStats["history"] => {
     const history: IGameStats["history"] = [];
     for (let i = 0; i < mode.rows; i++) {
-      history.push({ attempt: i, count: 0 });
+      history.push({ attempt: i + 1, count: 0 });
     }
+    history.push({ attempt: "X", count: 0 });
     return history;
   };
 
